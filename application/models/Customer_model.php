faa545689bd9ec927e41928f9bba2ea661b67695
Marwa Majidah <marwa.nbstar@gmail.com>
2019-12-02 01:32:47 -0800

    Update Customer_model.php

--- application/models/Customer_model.php
+++ application/models/Customer_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Home_model extends CI_Model {
+class Customer_model extends CI_Model {
 
   public function getObat($kode_obat = FALSE){
     if($kode_obat == FALSE){
